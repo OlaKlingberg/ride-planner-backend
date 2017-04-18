9484309f2f318bb96dbdc38555ddc0ecabb334b5
Ola Klingberg <ola@olaklingberg.com>
2017-04-18 13:01:04 -0400

    Sets and removes markers for logged-in riders -- but all get the user's coordinates.

--- server/socket-server.js
+++ server/socket-server.js
@@ -8,6 +8,8 @@ module.exports = class SocketServer {
 
     io.on('connection', (socket) => {
       console.log("io.on('connection')");
+
+      // Auction
       socket.emit('priceUpdate',currentPrice);
       socket.on('bid', function (data) {
         currentPrice = parseInt(data);
@@ -15,11 +17,25 @@ module.exports = class SocketServer {
         socket.broadcast.emit('priceUpdate',currentPrice);
       });
 
+      // Rider Map 2
+      socket.emit('newRiderList', riders);
+
       socket.on('newRider', (newRider, callback) => {
         console.log("io.on('newRider')");
         riders.push(newRider);
-        io.emit('updateRiders', riders);
+        io.emit('newRiderList', riders);
+        console.log(riders);
         callback();
+      });
+
+      socket.on('removeRider', (user, callback) => {
+        console.log('removeRider. About to remove', user);
+        // console.log(user);
+        console.log(riders);
+        riders = riders.filter(rider => rider.email !== user.email);
+        console.log('removeRider. Should have removed');
+        console.log(riders);
+        io.emit('newRiderList', riders);
       });
 
 
